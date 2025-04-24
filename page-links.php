550cfe92067f1f7edbb3f4d1397790ab5d31c10f
stsecurity <st.stsec@gmail.com>
2025-04-24 10:41:09 +0800

    Revert "Update page-links.php"
    This reverts commit 72662750695e324c491e35505b92af87f8130d5a.

--- page-links.php
+++ page-links.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * 友情链接页面
  *
@@ -78,7 +77,7 @@ $this->need('header.php'); ?>
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
     }
 
-    .md-links-item a:hover {
+    .md-links-item a:hover{
         cursor: pointer;
     }
 
@@ -87,11 +86,10 @@ $this->need('header.php'); ?>
         line-height: 50px;
     }
 
-    #scheme-Paradox .mdl-mini-footer {
+    #scheme-Paradox .mdl-mini-footer{
         clear: left;
     }
-
-    #bottom {
+    #bottom{
         position: relative;
     }
 </style>
@@ -99,122 +97,18 @@ $this->need('header.php'); ?>
 <div class="material-layout  mdl-js-layout has-drawer is-upgraded">
 
     <main class="material-layout__content" id="main">
-        <div class="min-height-for-footer">
-            <!-- Top Anchor -->
-            <div id="top"></div>
+      <div class="min-height-for-footer">
+        <!-- Top Anchor -->
+        <div id="top"></div>
 
-            <!-- Hamburger Button -->
-            <button class="MD-burger-icon sidebar-toggle">
-                <span id="MD-burger-id" class="MD-burger-layer"></span>
-            </button>
-        </div>
-        <!-- Post module -->
-        <div class="min-height-for-footer">
-            <div class="material-post_container">
-                <div class="material-post mdl-grid">
+        <!-- Hamburger Button -->
+        <button class="MD-burger-icon sidebar-toggle">
+            <span id="MD-burger-id" class="MD-burger-layer"></span>
+        </button>
 
-                    <!-- Article title -->
-                    <div class="mdl-card mdl-shadow--4dp mdl-cell mdl-cell--12-col">
-                        <div class="post_thumbnail-custom mdl-card__media mdl-color-text--grey-50" style="background-image: url(<?php echo showThumbnail($this); ?>);">
-                            <p class="article-headline-p">
-                                <?php $this->title() ?>
-                            </p>
-                        </div>
-
-                        <!-- Article info -->
-                        <div class="mdl-color-text--grey-700 mdl-card__supporting-text meta">
-                            <!-- Author avatar -->
-                            <div id="author-avatar">
-                                <?php if (!empty($this->options->avatarURL)): ?>
-                                    <img src="<?php $this->options->avatarURL() ?>" width="44px" height="44px" alt="Author Avatar">
-                                <?php else: ?>
-                                    <?php $this->author->gravatar(44); ?>
-                                <?php endif; ?>
-                            </div>
-
-                            <div>
-                                <!-- Author name -->
-                                <strong><?php $this->author(); ?></strong>
-                                <!-- Article date -->
-                                <span>
-                                    <?php if ($this->options->langis == '0'): ?>
-                                        <?php $this->date('F j, Y'); ?>
-                                    <?php else: ?>
-                                        <?php $this->dateWord(); ?>
-                                    <?php endif; ?>
-                                </span>
-                            </div>
-                            <div class="section-spacer"></div>
-                            <?php if (getThemeOptions("qrcode") != "false"): ?>
-                                <button id="article-functions-qrcode-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                                    <i class="material-icons" role="presentation">devices other</i>
-                                    <span class="visuallyhidden">devices other</span>
-                                </button>
-                                <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="article-functions-qrcode-button">
-                                    <li class="mdl-menu__item"><?php lang("post.qrcode") ?></li>
-                                    <img src="<?php getQRCode($this->permalink); ?>" height="200" width="200">
-                                </ul>
-                            <?php endif; ?>
-                            <!-- view tags -->
-                            <?php if (count($this->tags)): ?>
-                                <button id="article-functions-viewtags-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                                    <!-- For modern browsers. -->
-                                    <i class="material-icons" role="presentation">bookmarks</i>
-                                    <span class="visuallyhidden">tags</span>
-                                </button>
-                                <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="article-functions-viewtags-button">
-                                    <li class="mdl-menu__item">
-                                        <?php $this->tags('<li class="mdl-menu__item" style="text-decoration: none;"> ', true, ''); ?></li>
-                                </ul>
-                            <?php endif; ?>
-                            <!-- share -->
-                            <button id="article-fuctions-share-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                                <i class="material-icons" role="presentation">share</i>
-                                <span class="visuallyhidden">share</span>
-                            </button>
-                            <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="article-fuctions-share-button">
-                                <?php if ($this->user->hasLogin()): ?>
-                                    <a class="md-menu-list-a" target="_blank" href="<?php $this->options->adminUrl(); ?>write-page.php?cid=<?php echo $this->cid; ?>" target="_blank">
-                                        <li class="mdl-menu__item">编辑</li>
-                                    </a>
-                                <?php endif; ?>
-                                <a class="md-menu-list-a" href="https://www.facebook.com/sharer/sharer.php?u=<?php $this->permalink(); ?>">
-                                    <li class="mdl-menu__item">
-                                        <?php lang("share.toFacebook") ?>
-                                    </li>
-                                </a>
-                                <a class="md-menu-list-a" href="https://telegram.me/share/url?url=<?php $this->permalink() ?>&text=<?php $this->title(); ?>">
-                                    <li class="mdl-menu__item">
-                                        <?php lang("share.toTelegram") ?>
-                                    </li>
-                                </a>
-                                <a class="md-menu-list-a" href="https://twitter.com/intent/tweet?text=<?php $this->title() ?>&url=<?php $this->permalink() ?>&via=<?php $this->user->screenName(); ?>">
-                                    <li class="mdl-menu__item">
-                                        <?php lang("share.toTwitter") ?>
-                                    </li>
-                                </a>
-                                <a class="md-menu-list-a" href="http://service.weibo.com/share/share.php?appkey=&title=<?php $this->title(); ?>&url=<?php $this->permalink(); ?>&pic=&searchPic=false&style=simple ">
-                                    <li class="mdl-menu__item">
-                                        <?php lang("share.toWeibo") ?>
-                                    </li>
-                                </a>
-                            </ul>
-                        </div>
-
-                        <!-- Article content -->
-                        <div id="post-content" class="mdl-color-text--grey-700 mdl-card__supporting-text fade out">
-                            <?php
-                            if (!empty($this->options->switch) && in_array('PanguPHP', $this->options->switch)) {
-                                print pangu($this->content);
-                            } else {
-                                $this->content();
-                            }
-                            ?>
-                        </div>
-                        <!-- Links -->
-                        <div class="md-links">
-                            <?php if (class_exists("Links_Plugin")): ?>
-                                <?php Links_Plugin::output('
+        <div class="md-links">
+            <?php if (class_exists("Links_Plugin")): ?>
+                <?php Links_Plugin::output('
                 <a href="{url}" title="{title}" target="_blank">
                     <li class="md-links-item">
                         <img src="{image}" alt="{name}" height="72px"/>
@@ -223,28 +117,9 @@ $this->need('header.php'); ?>
                     </li>
                 </a>
                 '); ?>
-                            <?php endif; ?>
-                        </div>
-
-                        <!-- Article comments -->
-                        <?php $this->need('comments.php'); ?>
-                    </div>
-
-                    <!-- theNext thePrev button -->
-                    <nav class="material-nav mdl-color-text--grey-50 mdl-cell mdl-cell--12-col">
-                        <?php $this->theNext('%s', null, array('title' => '
-                        <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon mdl-color--white mdl-color-text--grey-900" role="presentation">
-                            <i class="material-icons">arrow_back</i>
-                        </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Newer', 'tagClass' => 'prev-content')); ?>
-                        <div class="section-spacer"></div>
-                        <?php $this->thePrev('%s', null, array('title' => 'Older&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon mdl-color--white mdl-color-text--grey-900" role="presentation">
-                            <i class="material-icons">arrow_forward</i>
-                        </button>', 'tagClass' => 'prev-content')); ?>
-                    </nav>
-                </div>
-            </div>
+            <?php endif; ?>
         </div>
 
-</div>
-<?php $this->need('sidebar.php'); ?>
-<?php $this->need('footer.php'); ?>
+        </div>
+        <?php $this->need('sidebar.php'); ?>
+        <?php $this->need('footer.php'); ?>
